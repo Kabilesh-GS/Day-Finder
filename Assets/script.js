7da396beecc6c3458a4c4bce0f2ae02c73552355
Kabilesh-GS <kabileshgs@gmail.com>
2024-08-25 14:05:08 +0530

    Done with Basic JS structure

--- Assets/script.js
+++ Assets/script.js
@@ -3,9 +3,39 @@ let FindBtn = document.querySelector('.FindButton');
 
 FindBtn.addEventListener('click', () => {
   let Dateval = Dateipt.value;
-  console.log(Dateval);
   let date = new Date(Dateval);
   
-  console.log(date.getDay());
-  console.log(date.getMonth());
+  let day = date.getDay();
+
+  if(Dateval == null || Dateval == ''){
+    alert('enter date');
+  }
+
+  else if(day == 0){
+    console.log('Sunday');
+  }
+  
+  else if(day == 1){
+    console.log('Monday');
+  }
+  
+  else if(day == 2){
+    console.log('Tuesday');
+  }
+  
+  else if(day == 3){
+    console.log('Wednesday');
+  }
+  
+  else if(day == 4){
+    console.log('Thursday');
+  }
+  
+  else if(day == 5){
+    console.log('Friday');
+  }
+  
+  else if(day == 6){
+    console.log('Saturday');
+  }
 });
