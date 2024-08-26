e7ebef88127e64d5b3ed8c135e89cfb3a02d1f58
Kabilesh-GS <kabileshgs@gmail.com>
2024-08-26 11:41:28 +0530

    Added month in output

--- Assets/script.js
+++ Assets/script.js
@@ -7,9 +7,12 @@ let ErrorDis = document.querySelector('.Error');
 FindBtn.addEventListener('click', () => {
   let Dateval = Dateipt.value;
   let date = new Date(Dateval);
-  
+
   let day = date.getDay();
   let Givendate = date.getDate();
+  let GivenMonth = date.getMonth();
+
+  const monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
   if(Dateval == null || Dateval == ''){
     ErrorDis.innerHTML = `Enter a Date`;
@@ -19,30 +22,30 @@ FindBtn.addEventListener('click', () => {
   }
 
   else if(day == 0){
-    DisplayResult.innerHTML = `${Givendate}<sup>th</sup> is Sunday !`;
+    DisplayResult.innerHTML = `${Givendate}<sup>th</sup> of ${monthArr[GivenMonth]} is Sunday !`;
   }
   
   else if(day == 1){
-    DisplayResult.innerHTML = `${Givendate}<sup>th</sup> is Monday !`;
+    DisplayResult.innerHTML = `${Givendate}<sup>th</sup> of ${monthArr[GivenMonth]} is Monday !`;
   }
   
   else if(day == 2){
-    DisplayResult.innerHTML = `${Givendate}<sup>th</sup> is Tuesday !`;
+    DisplayResult.innerHTML = `${Givendate}<sup>th</sup> of ${monthArr[GivenMonth]} is Tuesday !`;
   }
   
   else if(day == 3){
-    DisplayResult.innerHTML = `${Givendate}<sup>th</sup> is Wednesday !`;
+    DisplayResult.innerHTML = `${Givendate}<sup>th</sup> of ${monthArr[GivenMonth]} is Wednesday !`;
   }
   
   else if(day == 4){
-    DisplayResult.innerHTML = `${Givendate}<sup>th</sup> is Thursday !`;
+    DisplayResult.innerHTML = `${Givendate}<sup>th</sup> of ${monthArr[GivenMonth]} is Thursday !`;
   }
   
   else if(day == 5){
-    DisplayResult.innerHTML = `${Givendate}<sup>th</sup> is Friday !`;
+    DisplayResult.innerHTML = `${Givendate}<sup>th</sup> of ${monthArr[GivenMonth]} is Friday !`;
   }
   
   else if(day == 6){
-    DisplayResult.innerHTML = `${Givendate}<sup>th</sup> is Saturday !`;
+    DisplayResult.innerHTML = `${Givendate}<sup>th</sup> of ${monthArr[GivenMonth]} is Saturday !`;
   }
 });
